7b2a1422b87553bfa260464d688bea082e6a98ea
kensak222 <lune.abc@gmail.com>
2024-11-27 17:32:15 +0900

    add: 動画再生機能を追加

--- lib/ui/pose-estimation-network/video_player_screen.dart
+++ lib/ui/pose-estimation-network/video_player_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:video_player/video_player.dart';
-
 import '../../util/utils.dart';
 
 class VideoPlayerScreen extends HookWidget {
@@ -13,13 +12,23 @@ class VideoPlayerScreen extends HookWidget {
   @override
   Widget build(BuildContext context) {
     logger.d("動画再生を開始します videoPath : $videoPath");
+
     final controller = useMemoized(
         () => VideoPlayerController.networkUrl(Uri.parse(videoPath)));
     final initialized = useState(false);
+    final isPlaying = useState(false);
 
     useEffect(() {
       controller.initialize().then((_) {
         initialized.value = true;
+        // 初期化完了後に自動で再生
+        controller.play();
+      });
+      controller.addListener(() {
+        // 再生状態の変更を監視する
+        if (controller.value.isPlaying != isPlaying.value) {
+          isPlaying.value = controller.value.isPlaying;
+        }
       });
       return controller.dispose;
     }, [controller]);
@@ -28,9 +37,29 @@ class VideoPlayerScreen extends HookWidget {
       return const Center(child: CircularProgressIndicator());
     }
 
-    return AspectRatio(
-      aspectRatio: controller.value.aspectRatio,
-      child: VideoPlayer(controller),
+    return Stack(
+      children: [
+        AspectRatio(
+          aspectRatio: controller.value.aspectRatio,
+          child: VideoPlayer(controller),
+        ),
+        // 再生していない場合に再生ボタンを表示
+        if (!isPlaying.value)
+          Center(
+            child: IconButton(
+              icon: const Icon(
+                Icons.play_arrow,
+                size: 64,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                logger.d('再生ボタンがタップされました');
+                controller.play();
+                isPlaying.value = true;
+              },
+            ),
+          ),
+      ],
     );
   }
 }
